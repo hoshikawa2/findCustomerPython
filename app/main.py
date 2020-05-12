629b1455cb46d4c6ec51ea49bf162f502c1c87e0
Cristiano Hoshikawa <hoshikawa@uol.com.br>
2020-05-12 10:41:33 -0300

    first commit

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/findCustomer/<string:nome>/<string:cpf>/<string:cartao>", methods=['GET'])
+def findCustomer(nome, cpf, cartao):
+    nomeCompleto = "Cristiano Hoshikawa";
+    plano = "Plano Super";
+    return jsonify({'nomeCompleto': nomeCompleto, 'plano': plano});
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
